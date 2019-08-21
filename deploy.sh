c40a7d5fb11e40a0f6c91ffae1945ecbaf4abba7
vyshakg <vyshak.g.gowda@gmail.com>
2019-08-21 23:58:05 +0530

    made changed and build it and deploying it ---- automated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,1 @@
-#! /bin/bash
-yarn build
-git commit -am "made changed and build it and deploying it ---- automated"
-git push -u origin master
+yarn build && git commit -am "made changed and build it and deploying it ---- automated" && git push -u origin master
